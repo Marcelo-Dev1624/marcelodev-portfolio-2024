66865e528656e4e0fa4e1f8b01893f57b72e5a82
MarceloVA <100235694+Marcelo-Dev1624@users.noreply.github.com>
2024-09-13 11:51:11 -0600

    images not showing on mobile
    now to test

--- js/loadingAnimation.js
+++ js/loadingAnimation.js
@@ -2,7 +2,8 @@
 const loadAnimation = document.getElementById("onload-animation");
 const animationLogoWhite = document.getElementById("animation-logo-white");
 const animationLogoGreen = document.getElementById("animation-logo-green");
-const html = document.querySelector('html');
+//const animationText = document.getElementById("animationText");
+const html = document.querySelector("html");
 
 window.addEventListener("load", () => {
   setTimeout(() => {
@@ -10,21 +11,20 @@ window.addEventListener("load", () => {
   }, 1000);
 
   setTimeout(() => {
-    animationLogoWhite.classList.add('fadeOut');
-    animationLogoGreen.classList.add('fadeIn');
+    animationLogoWhite.classList.add("fadeOut");
+    animationLogoGreen.classList.add("fadeIn");
+   // animationText.classList.add("fadeIn");
   }, 2500);
 
-  setTimeout(() => {
-    
-  }, 3000);
 
   setTimeout(() => {
     animationLogoGreen.classList.add("fadeOut");
-    loadAnimation.classList.add("onload");
-    
-  }, 4000);
+    //animationText.classList.add("fadeOut");
+    setTimeout(() => {
+      loadAnimation.classList.add("onload");
+    }, 500);
+  }, 3500);
+  
 });
 
-setTimeout(() => {
-    html.classList.remove('onload');
-}, 5000);
+
